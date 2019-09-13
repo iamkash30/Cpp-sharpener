4b9337e3ea8db143826d4f4b53a17d45169b04a8
Aakash Chitroda <aakash.chitroda@hotmail.com>
2019-09-12 20:10:34 -0700

    Added leetcode problem no 217

--- leetcode/217/main.cpp
+++ leetcode/217/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <map>
+#include <set>
+#include <vector>
 
 using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        if (nums.empty()) return false;
+        size_t len = nums.size();
+        if (len < 2) return false;
+        int i,j;
+
+        set<int> s, sd;
+        for (auto &i : nums) {
+            if (true == s.insert(i).second) {}
+            else if (true == sd.insert(i).second)
+                return true;
+        }
+        return false;
+    }
+};
 
 int main()
 {
