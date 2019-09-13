f92d676f5611cc849920381e784eeda1526bd38a
Aakash Chitroda <aakash.chitroda@hotmail.com>
2019-09-12 20:10:11 -0700

    Added leetcode problem no 169

--- leetcode/169/main.cpp
+++ leetcode/169/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <map>
+#include <set>
 
 using namespace std;
 
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> m;
+        for (int i : nums) {
+            m[i]++;
+        }
+
+        for (auto it = m.cbegin(); it != m.cend(); it++) {
+            if ( it->second > nums.size()/2)
+                return it->first;
+        }
+    }
+};
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    Solution s;
+    vector<int> num;
+    num.push_back(2);
+    num.push_back(1);
+    num.push_back(1);
+    num.push_back(2);
+    num.push_back(2);
+    num.push_back(2);
+
+    cout << s.majorityElement(num) << endl;
+
+
     return 0;
 }
