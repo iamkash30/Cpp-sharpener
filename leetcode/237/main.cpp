f1360eca3b5e334af4de7f7a2d1a60729afc694f
Aakash Chitroda <aakash.chitroda@hotmail.com>
2019-09-12 20:10:53 -0700

    Added leetcode problem no 237

--- leetcode/237/main.cpp
+++ leetcode/237/main.cpp
@@ -2,8 +2,33 @@
 
 using namespace std;
 
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ListNode *head;
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    head = new ListNode(4);
+    head->next = new ListNode(5);
+    head->next->next = new ListNode(1);
+    head->next->next->next = new ListNode(9);
+    Solution s;
+    ListNode *node = new ListNode(5);
+    cout << head << endl;
+    cout << node << endl;
+
     return 0;
 }
